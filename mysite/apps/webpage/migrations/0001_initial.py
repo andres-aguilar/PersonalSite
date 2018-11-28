d8aaf976937cabe2bbdeebf8f7dd5b435057cf21
Andrés Aguilar <andresyoshimar@gmail.com>
2018-11-28 14:53:31 -0600

    Creando primer modelo de proyectos con sus migraciones

---
+++ mysite/apps/webpage/migrations/0001_initial.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='MyProjects',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
+                ('name', models.CharField(max_length=50)),
+                ('description', models.TextField()),
+                ('icon', models.CharField(max_length=25)),
+                ('url', models.URLField()),
+            ],
+        ),
+    ]
--- mysite/apps/webpage/models.py
+++ mysite/apps/webpage/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class MyProjects(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    icon = models.CharField(max_length=25)
+    url = models.URLField()
+
+    def __str__(self):
+        return self.name
