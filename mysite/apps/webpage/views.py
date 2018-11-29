a0e3561a9df160a15d029731ac63e876c0273e2c
Andrés Aguilar <andresyoshimar@gmail.com>
2018-11-28 21:42:31 -0600

    Agregando listado de proyectos al index

--- mysite/apps/webpage/views.py
+++ mysite/apps/webpage/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 
+from .models import MyProjects
 
 def under_construction(request):
     return render(request, 'under_construction.html')
 
 
 def index(request):
-    return render(request, 'webpage/index.html')
+    projects = MyProjects.objects.all()
+    context = {
+        'projects': projects
+    }
+    return render(request, 'webpage/index.html', context)
