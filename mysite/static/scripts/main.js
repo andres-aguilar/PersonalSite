40f963589f8387169c0356084f6a2b0ce4285f69
Andrés Aguilar <andresyoshimar@gmail.com>
2018-11-30 08:11:29 -0600

    Cambiando las imágenes y arreglando menú responsivo

--- mysite/static/scripts/main.js
+++ mysite/static/scripts/main.js
@@ -1,4 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.parallax')
-    var instances = M.Parallax.init(elems, {})
-  })
+	var elems = document.querySelectorAll('.parallax')
+	var instances = M.Parallax.init(elems, {})
+
+	var side = document.querySelectorAll('.sidenav')
+    var side_instances = M.Sidenav.init(side, {})
+})
