c5e04fb28ca6ad5fbc7083e2a611bc821e533903
Andrés Aguilar <andresyoshimar@gmail.com>
2018-12-27 17:15:04 -0600

    Creando y registrando la app de blog

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     # Local apps
     'apps.webpage',
+    'apps.blog'
 )
 
 MIDDLEWARE_CLASSES = (
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import include, url
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^blog/', include('apps.blog.urls', namespace='blog')),
     url(r'^', include('apps.webpage.urls', namespace='website')),
 ]
