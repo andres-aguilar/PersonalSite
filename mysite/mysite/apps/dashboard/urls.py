223422195111bc6668d0e5ed366e87551bf273a0
Andrés Aguilar <andresyoshimar@gmail.com>
2019-01-11 13:27:45 -0600

    Habilitando la edición de artículos del blog y refactorizando código

--- mysite/mysite/apps/dashboard/urls.py
+++ mysite/mysite/apps/dashboard/urls.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import url
 
-from .views import index, CreatePost
+from .views import index, CreatePost, UpdatePost
 
 urlpatterns = [
     url(r'^$', index, name='index'),
-    url(r'^new_post/$', CreatePost.as_view(), name="new_post")
+    url(r'^new_post/$', CreatePost.as_view(), name="new_post"),
+    url(r'^edit/(?P<slug>[-\w]+)/$', UpdatePost.as_view(), name="edit_post"),
 ]
--- mysite/mysite/apps/dashboard/views.py
+++ mysite/mysite/apps/dashboard/views.py
@@ -2,8 +2,8 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from django.views.generic import CreateView
 from django.core.urlresolvers import reverse_lazy
+from django.views.generic import CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 
@@ -20,13 +20,30 @@ def index(request):
 
 class CreatePost(LoginRequiredMixin, CreateView):
     login_url = 'login'
-    template_name = 'dashboard/new_post.html'
+    template_name = 'dashboard/blog/new_post.html'
     model = Article
     form_class = ArticleForm
     success_url = reverse_lazy('dashboard:index')
 
     def get_context_data(self, **kwargs):
         context = super(CreatePost, self).get_context_data(**kwargs)
+        context["user"] = Profile.objects.get(pk=1)
+        context["social_medias"] = SocialMedia.objects.all()
+        return context
+
+
+class UpdatePost(LoginRequiredMixin, UpdateView):
+    login_url = 'login'
+    template_name = 'dashboard/blog/new_post.html'
+    model = Article
+    form_class = ArticleForm
+
+    def get_success_url(self, **kwargs):
+        return reverse_lazy('blog:view', kwargs={'slug': self.kwargs['slug']})
+
+
+    def get_context_data(self, **kwargs):
+        context = super(UpdatePost, self).get_context_data(**kwargs)
         context["user"] = Profile.objects.get(pk=1)
         context["social_medias"] = SocialMedia.objects.all()
         return context
